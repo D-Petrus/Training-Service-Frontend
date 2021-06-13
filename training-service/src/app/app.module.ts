09d3bc09407ada5693daf458a8e58272a34d21c7
PES34 <4headx@gmail.com>
2021-06-13 14:35:04 +0200

    add footer

--- training-service/src/app/app.module.ts
+++ training-service/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ContentComponent } from './pages/content/content.component';
 import { CategoryListComponent } from './content/category/component/category-list/category-list.component';
 import { CategoryService } from './content/category/service/category.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContentComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
