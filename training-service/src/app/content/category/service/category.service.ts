aff0d63f597327df40ddc31d6558231ab4d13ce2
mb83pl <mbutora@gmail.com>
2021-06-13 14:08:43 +0200

    getting list of categories

--- training-service/src/app/content/category/service/category.service.ts
+++ training-service/src/app/content/category/service/category.service.ts
@@ -1,7 +1,18 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Category } from '../model/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+
+  private readonly apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getCategoryList = (): Observable<Category[]> =>
+    this.http.get<Category[]>(`${this.apiUrl}/categories`);
 }
