61dd176f322d86c1b23ae5d713028d48671ed152
mb83pl <mbutora@gmail.com>
2021-06-13 15:23:43 +0200

    adding categories

---
+++ training-service/src/app/content/category/component/category-add/category-add.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Category } from '../../model/category';
+import { CategoryService } from '../../service/category.service';
+
+@Component({
+  selector: 'app-category-add',
+  templateUrl: './category-add.component.html',
+  styleUrls: ['./category-add.component.css']
+})
+export class CategoryAddComponent implements OnInit {
+
+  category!: Category;
+  submitted = false;
+
+  categoryAddForm = new FormGroup({
+    id: new FormControl(''),
+    nameCategory: new FormControl('', [
+      Validators.required,
+    ]),
+    description: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+  });
+
+  constructor(private categoryService: CategoryService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  saveCategory() {
+    const cat: Category = this.categoryAddForm.value;
+    this.categoryService.addCategory(cat).subscribe(() => {
+      this.categoryService.getCategoryList();
+    });
+    this.categoryAddForm.reset();
+    this.submitted = true;
+  }
+
+  getBack() {
+    this.router.navigate(['/books']);
+  }
+}
--- training-service/src/app/content/category/service/category.service.ts
+++ training-service/src/app/content/category/service/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Category } from '../model/category';
 
@@ -11,8 +12,38 @@ export class CategoryService {
 
   private readonly apiUrl: string = environment.apiUrl;
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': `${this.apiUrl}`,
+    }),
+  };
+
+  msgTrue = false;
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.log('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.log(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
+  }
+
   constructor(private http: HttpClient) {}
 
   getCategoryList = (): Observable<Category[]> =>
     this.http.get<Category[]>(`${this.apiUrl}/categories`);
+
+  addCategory = (category: Category): Observable<Category> => {
+  let catBody = JSON.stringify(category);
+    return this.http.post<Category>(`${this.apiUrl}/categories`, catBody, this.httpOptions)
+    .pipe(catchError(this.handleError));
+  }
 }
