3f230b7eae037010346718494a88aa559f697a54
mbutora@gmail.com <Doctype1983!>
2021-08-08 12:11:22 +0200

    update

--- training-service/src/app/content/category/component/category-list/category-list.component.ts
+++ training-service/src/app/content/category/component/category-list/category-list.component.ts
@@ -8,7 +8,7 @@ import { CategoryService } from '../../service/category.service';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.scss']
 })
 export class CategoryListComponent implements OnInit {
   isShow = false;
