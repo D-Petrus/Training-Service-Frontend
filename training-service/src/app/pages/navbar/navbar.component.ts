e8374958c53f950e087e87ef1f79063a0f617f10
mbutora@gmail.com <Doctype1983!>
2021-08-08 11:46:02 +0200

    hamburger menu

--- training-service/src/app/pages/navbar/navbar.component.ts
+++ training-service/src/app/pages/navbar/navbar.component.ts
@@ -6,4 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isCollapsed = true;
+
+  clickMenu = () => this.isCollapsed = !this.isCollapsed;
 }
