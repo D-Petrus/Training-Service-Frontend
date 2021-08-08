fc55b5fb4b33a4dfa9725aeba6b9e2abd4fb998d
mbutora@gmail.com <Doctype1983!>
2021-08-08 12:04:06 +0200

    update

--- training-service/src/app/content/category/component/category-name/category-name.component.spec.ts
+++ training-service/src/app/content/category/component/category-name/category-name.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { CategoryNameComponent } from './category.component';
+import { CategoryNameComponent } from './category-name.component';
 
 describe('CategoryNameComponent', () => {
   let component: CategoryNameComponent;
