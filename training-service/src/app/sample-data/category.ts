108416abbac86924b2b8a12ccc453cd5568cd916
neoed <neoed@tlen.pl>
2021-08-28 09:43:53 +0200

    mock category data

---
+++ training-service/src/app/sample-data/category.ts
@@ -0,0 +1,24 @@
+import {Category} from "../content/category/model/category";
+
+export const category: Category[] = [
+    {
+        id:1,
+        name:"IT",
+        description:"lorem ipsum"
+    },
+    {
+        id:2,
+        name:"Sales",
+        description:"coś tam coś tam"
+    },
+    {
+        id:3,
+        name:"Marketing",
+        description:"lorem ipsum2"
+    },
+    {
+        id:4,
+        name:"Other",
+        description:"ugabuga 40 i 4"
+    }
+]
