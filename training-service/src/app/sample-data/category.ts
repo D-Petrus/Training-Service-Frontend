9ef5f7715377d986c2d8d19b7fe24602054bed58
neoed <neoed@tlen.pl>
2021-08-28 09:47:34 +0200

    mock category data without description

--- training-service/src/app/sample-data/category.ts
+++ training-service/src/app/sample-data/category.ts
@@ -3,22 +3,18 @@ import {Category} from "../content/category/model/category";
 export const category: Category[] = [
     {
         id:1,
-        name:"IT",
-        description:"lorem ipsum"
+        name:"IT"
     },
     {
         id:2,
-        name:"Sales",
-        description:"coś tam coś tam"
+        name:"Sales"
     },
     {
         id:3,
-        name:"Marketing",
-        description:"lorem ipsum2"
+        name:"Marketing"
     },
     {
         id:4,
-        name:"Other",
-        description:"ugabuga 40 i 4"
+        name:"Other"
     }
 ]
