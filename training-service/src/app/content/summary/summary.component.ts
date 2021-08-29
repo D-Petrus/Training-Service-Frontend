f2ad41c7f25cb6be32ddf01caf65171af7036c6f
D-Petrus <damianpetrus@gmail.com>
2021-08-29 13:24:18 +0200

    summary-view fix

--- training-service/src/app/content/summary/summary.component.ts
+++ training-service/src/app/content/summary/summary.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../course/service/course.service';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent {
+  faCheck = faCheck;
 
 coursesSelected: string[] = [];
 email: string = '';
