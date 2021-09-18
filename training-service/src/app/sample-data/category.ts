209986925f2aa7f223b8b332dfb8f96cefe2027a
mbutora@gmail.com <Doctype1983!>
2021-09-18 09:39:57 +0200

    some fixes

--- training-service/src/app/sample-data/category.ts
+++ training-service/src/app/sample-data/category.ts
@@ -1,20 +1,2 @@
-import {Category} from "../content/category/model/category";
+import {Category} from '../content/category/model/category';
 
-export const category: Category[] = [
-    {
-        id:1,
-        name:"IT"
-    },
-    {
-        id:2,
-        name:"Sales"
-    },
-    {
-        id:3,
-        name:"Marketing"
-    },
-    {
-        id:4,
-        name:"Other"
-    }
-]
